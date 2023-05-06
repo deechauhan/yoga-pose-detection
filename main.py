2d09351601a0b2a70f9e314fe0dd57468662364e
Deepak Chauhan <127858338+deechauhan@users.noreply.github.com>
2023-05-06 22:07:32 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+"""Main camera launcher.
+
+This launcher should be able to:
+    1. Choose from multiple cameras
+    2. Call any dependency to add overlay in realtime
+"""
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+if not cap.isOpened():
+    print("Error: Could not open camera")
+    exit()
+
+while True:
+    ret, original_frame = cap.read()
+    if not ret:
+        print("Error: Could not read frame from camera")
+        break
+
+    # Mirror each frame
+    mirrored_frame = cv2.flip(original_frame, 1)
+    cv2.imshow("Camera", mirrored_frame)
+
+    key = cv2.waitKey(1)
+    if key == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
